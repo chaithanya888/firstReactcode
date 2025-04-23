f2c24cbda9479d27ed06b5c72c48f80b60030760
your_username <kopparthichaithanyaraju@gmail.com>
2025-04-23 16:02:27 +0530

    used react

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+
+function Header(){
+
+    return( 
+        <header>
+            <h1>my website</h1>
+            <ul className="list">
+                <li><a herf="">home</a></li>
+                <li><a herf="">service</a></li>
+                <li><a herf="">about</a></li>
+            </ul>
+        </header>
+    );
+   }
+   
+   export default Header
