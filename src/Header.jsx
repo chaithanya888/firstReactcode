a01801615728a2ea223a7394a3f5993955e39679
your_username <kopparthichaithanyaraju@gmail.com>
2025-04-25 15:06:54 +0530

    used object in react

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,18 @@
 
 function Header(){
-
+const profile={
+    student:"name",
+    id:78,
+    gender:"male",
+    class:10,
+};
     return( 
         <header>
             <h1>my website</h1>
             <ul className="list">
-                <li><a herf="">home</a></li>
-                <li><a herf="">service</a></li>
-                <li><a herf="">about</a></li>
+                <li><a herf="">{profile.student}</a></li>
+                <li><a herf="">{profile.id}</a></li>
+                <li><a herf="">{profile.class}</a></li>
             </ul>
         </header>
     );
